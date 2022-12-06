60807b1b25af5d1e9e947e52601ee5ca9d42e51d
niko <66620705+nikoLua@users.noreply.github.com>
2022-12-06 14:16:34 +0800

    Add files via upload

---
+++ value_reference_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace value_reference_1
+{
+    class Program
+    {
+    
+        static void func(ref int[] array)
+        {
+            array[0] = 888;
+            array = new int[3];
+            array[0] = 999;
+        }
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 2, 3 };
+            func(ref array);
+            Console.WriteLine(array[0]);
+        }
+    }
+}
